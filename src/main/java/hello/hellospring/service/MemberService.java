2191794dbb0c94706ea9ee90a6dc66aea7c6b42c
NamSeok_DT <skatjr30@naver.com>
2021-07-18 04:12:19 +0900

    AOP - AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -21,20 +21,9 @@ public class MemberService {
      */
     public Long join(Member member){
 
-        long start = System.currentTimeMillis();
-
-        try{
-            validateDuplicateMember(member);
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
-
-
-
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -49,19 +38,7 @@ public class MemberService {
      */
 
     public List<Member> findMembers(){
-
-        long start = System.currentTimeMillis();
-
-        try{
-            return memberRepository.findAll();
-        }
-        finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
-
-
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
